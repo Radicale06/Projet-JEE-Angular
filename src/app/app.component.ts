f0ac4ccb944bb8857360ed8f4ae057753f997f99
kamaili aymen <kmailiaymen10@gmail.com>
2024-04-05 21:42:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'quizz_frontend';
+export class AppComponent implements OnInit{
+
+  active_page = 'student'
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  activateUserPage(userType: string){
+    this.active_page = userType;
+  }
+  
 }
